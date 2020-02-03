b19a2f81358a47f1b144855b667bc3a6ca3ae1c8
NARUSE, Yui <naruse@airemix.jp>
2020-02-04 02:29:30 +0900

    -u is not required because it specifies tag

--- config/software/datadog-gohai.rb
+++ config/software/datadog-gohai.rb
@@ -3,6 +3,8 @@ default_version "last-stable"
 
 always_build true
 
+dependency "go"
+
 env = {
   "GOPATH" => "#{Omnibus::Config.cache_dir}/src/#{name}",
 }
@@ -20,9 +22,9 @@ build do
   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/LICENSE"
   ship_license "https://raw.githubusercontent.com/DataDog/gohai/#{version}/THIRD_PARTY_LICENSES.md"
   # Checkout gohai's deps
-  command "#{gobin} get -u github.com/shirou/gopsutil", :env => env
+  command "#{gobin} get github.com/shirou/gopsutil", :env => env
   command "git checkout v2.0.0", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/shirou/gopsutil"
-  command "#{gobin} get -u github.com/cihub/seelog", :env => env
+  command "#{gobin} get github.com/cihub/seelog", :env => env
   command "git checkout v2.6", :env => env, :cwd => "#{Omnibus::Config.cache_dir}/src/datadog-gohai/src/github.com/cihub/seelog"
   # Windows depends on the registry, go get that.
   if ohai["platform"] == "windows"
